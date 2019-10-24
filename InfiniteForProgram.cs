c67827bda98dd99f1516a021bb06d2eeb607b7c3
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-25 00:33:33 +0200

    Added InfiniteForLoopProgram

---
+++ InfiniteForProgram.cs
@@ -0,0 +1,13 @@
+// Infinite For Loop Program
+
+using System;  
+public class InfiniteForProgram
+    {  
+      public static void Main(string[] args)  
+      {  
+          for (; ;)  
+          {  
+                  Console.WriteLine("Infinitive For Loop Program.");  
+          }    
+      }  
+ } 
--- NestedForProgram.cs
+++ NestedForProgram.cs
@@ -5,7 +5,7 @@ public class NestedForProgram
     {  
       public static void Main(string[] args)  
       {  
-        for(int a=1; a< = 5; a++){    
+        for(int a=1; a<= 5; a++){    
                 for(int b=1; b<=3; b++){    
                     Console.WriteLine(a+" "+b);    
                 }    
