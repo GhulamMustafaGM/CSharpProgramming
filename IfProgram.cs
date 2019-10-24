bf0e5de130d94a23299561a03834367d62958e9d
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-24 23:55:45 +0200

    Added IfProgram

---
+++ IfProgram.cs
@@ -0,0 +1,12 @@
+using System;
+class IfProgram 
+{
+    static void Main(string []args)
+    {
+        int Value = 10;
+
+        if (Value % 2 == 0) {
+        Console.WriteLine("It is even value");
+        }
+    }    
+}
