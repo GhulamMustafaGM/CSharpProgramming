6ac693bfde72c1a289f8604888c57cf90b62ee67
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-25 00:25:34 +0200

    Added NestedForLoopProgram

---
+++ NestedForProgram.cs
@@ -0,0 +1,14 @@
+// Nested For loop program
+
+using System;  
+public class NestedForProgram  
+    {  
+      public static void Main(string[] args)  
+      {  
+        for(int a=1; a< = 5; a++){    
+                for(int b=1; b<=3; b++){    
+                    Console.WriteLine(a+" "+b);    
+                }    
+        }    
+      }  
+    }  
