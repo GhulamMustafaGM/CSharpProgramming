472888616bcde3ab25a1dcd6dcf79f3ed11a9860
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-24 23:58:39 +0200

    Added IfElseProgram

---
+++ IfElseProgram.cs
@@ -0,0 +1,15 @@
+using System;
+class IfElseProgram 
+{
+    static void Main(string []args)
+    {
+        int Value = 13;
+
+        if (Value % 2 == 0) {
+        Console.WriteLine("It is even value");
+        }
+        else{
+            Console.WriteLine("It is odd value");
+        }
+    }    
+}
