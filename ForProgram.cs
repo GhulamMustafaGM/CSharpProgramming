1c40dbdad032fb063aa6937d60a6593d2b0a1642
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-25 00:21:07 +0200

    Added ForLoopProgram

---
+++ ForProgram.cs
@@ -0,0 +1,12 @@
+// For loop program
+
+using System;  
+public class ForProgram  
+    {  
+      public static void Main(string[] args)  
+      {  
+          for(int num=1; num<=15; num++){    
+            Console.WriteLine(num);    
+          }    
+      }  
+    }
