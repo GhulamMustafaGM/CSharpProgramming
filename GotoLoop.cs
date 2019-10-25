bbe22bce45b80e2701bbf0d8bb9dbf66dbfa8384
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-25 17:59:17 +0200

    GotoLoop

---
+++ GotoLoop.cs
@@ -0,0 +1,22 @@
+// Goto Loop Statement 
+
+using System;
+public class GotoLoop
+{
+    public static void Main(string[] args)
+    {
+    ineligible:
+        Console.WriteLine("You are not eligible to ID!");
+
+        Console.WriteLine("Enter your age:\n");
+        int age = Convert.ToInt32(Console.ReadLine());
+        if (age < 18)
+        {
+            goto ineligible;
+        }
+        else
+        {
+            Console.WriteLine("You are eligible to ID!");
+        }
+    }
+}
