83cfe282129a9a3fe5b7687af1288f330e642f98
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-25 16:03:45 +0200

    InfinitiveDoWhile

---
+++ InifinitiveDoWhileProgram.cs
@@ -0,0 +1,13 @@
+// NestedDoWhile Loop Program 
+
+using System;  
+public class InfinitiveDoWhileProgram
+    {  
+        
+      public static void Main(string[] args)  
+      {  
+          do {
+                Console.WriteLine("InfinitiveDoWhile Loop");
+          }while(true);
+     }
+ } 
