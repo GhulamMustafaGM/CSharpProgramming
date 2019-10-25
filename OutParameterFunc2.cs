4e188db7a22f679e6a26d60f06ab7114c0c3c7fd
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-25 18:49:55 +0200

    OutParameter Function2

---
+++ OutParameterFunc2.cs
@@ -0,0 +1,33 @@
+// Defined out parameter program2 
+
+using System;
+namespace OutParameterProgram2
+{
+    class Program
+    {
+        // User-defined function  
+        public void Show(out int a, out int b) // Defiend out parameter  
+        {
+            int square = 5;
+            i = square;
+            j = square;
+            
+            // calculating value  
+            i *= i;
+            j *= j;
+        }
+        // with main function, execution entry point of the program  
+        static void Main(string[] args)
+        {
+            int num1 = 50, num2 = 100;
+
+            Program program = new Program(); //  Object  
+
+            Console.WriteLine("Value before passing \n num1 = " + num1 + " \n num2 = " + num2);
+
+            program.Show(out num1, out num2); // Passing out argument  
+
+            Console.WriteLine("Value after passing \n num1 = " + num + " \n num2 = " + num2);
+        }
+    }
+}
