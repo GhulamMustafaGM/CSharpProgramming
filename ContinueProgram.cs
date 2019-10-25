1d6efcd3ee65b622144e60e964a2aa65141b1c90
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-25 17:49:52 +0200

    ContineProgram

---
+++ ContinueProgram.cs
@@ -0,0 +1,17 @@
+// Continue Statement Program 
+
+using System;
+public class ContinueExample
+{
+    public static void Main(string[] args)
+    {
+        for (int a = 1; a <= 10; i++)
+        {
+            if (a == 5)
+            {
+                continue;
+            }
+            Console.WriteLine(a);
+        }
+    }
+}
