18ba16b8ef89b2b520f0c67b37ba771544bb1c42
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-25 15:54:24 +0200

    DoWhile

---
+++ DoWhileProgram.cs
@@ -0,0 +1,17 @@
+// DoWhile Loop Program do { // code to be executed} while(condition);
+
+using System;  
+public class InfiniteForProgram
+    {  
+        
+      public static void Main(string[] args)  
+      {  
+          int a = 1;
+          do  
+          {  
+              Console.WriteLine(a);
+              a++;
+          }   
+          while(a <= 15);
+      }  
+ } 
