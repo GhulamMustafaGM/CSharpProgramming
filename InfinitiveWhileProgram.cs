b7b9b126855686d5800194420ef89d4295faa07c
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-25 15:50:53 +0200

    InfinitiveWhile

---
+++ InfinitiveWhileProgram.cs
@@ -0,0 +1,15 @@
+// InfinitivedWhile Loop Program
+
+using System;  
+public class InfiniteForProgram
+    {  
+        
+      public static void Main(string[] args)  
+      {  
+          
+          while (true)  
+          {  
+              Console.WriteLine("InfintiveWhile Loop Program. ");
+          }   
+      }  
+ } 
