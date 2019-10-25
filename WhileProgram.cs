81a8e42eaebf7a1711de02fa33d73141dda5cc13
Ghulam Mustafa <mustafaji@gmail.com>
2019-10-25 15:43:24 +0200

    WhileProgram

---
+++ WhileProgram.cs
@@ -0,0 +1,17 @@
+// While Loop Program // while(condition) {code to be executed.}
+
+using System;  
+public class InfiniteForProgram
+    {  
+        
+      public static void Main(string[] args)  
+      {  
+          int a = 1;
+
+          while (a <= 15 )  
+          {  
+                Console.WriteLine(a);  
+                a++;
+          }    
+      }  
+ } 
