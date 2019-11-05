3ac1617bcf1e8298afa21d781af310f58545fc92
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-05 20:15:30 +0100

    added class12 static field and method

---
+++ TestingApp/TestingApp/class12.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public static class MyCalculation
+{
+    public static float PI = 3.14f;
+    public static int cube(int n) { return n * n * n; }
+}
+class TestCalculation
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Value of PI is: " + MyCalculation.PI);
+        Console.WriteLine("Cube of 3 is: " + MyCalculation.cube(3));
+    }
+}
