eeca4397321b6666949b7768fb3952af25af4dff
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-05 20:37:20 +0100

    added class15: enum

---
+++ TestingApp/TestingApp/class16.cs
@@ -0,0 +1,16 @@
+﻿using System;
+public class EnumProgram
+{
+    public enum Season { WINTER, SPRING, SUMMER, FALL }
+
+    public static void Main()
+    {
+        int x = (int)Season.WINTER;
+        int y = (int)Season.SUMMER;
+        int z = (int)Season.SUMMER;
+
+        Console.WriteLine("WINTER = {0}", x);
+        Console.WriteLine("SUMMER = {0}", y);
+        Console.WriteLine("Fall = {0}" , z);
+    }
+}
