092c16df2c5afa7be2fab451e234686c2dcc618e
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-05 20:41:25 +0100

    added class17: enum changing start index

---
+++ TestingApp/TestingApp/class17.cs
@@ -0,0 +1,16 @@
+﻿using System;
+public class EnumProgram2
+{
+    public enum Season { WINTER = 10, SPRING, SUMMER, FALL }
+
+    public static void Main()
+    {
+        int x = (int)Season.WINTER;
+        int y = (int)Season.SUMMER;
+        int z = (int)Season.FALL;
+
+        Console.WriteLine("WINTER = {0}", x);
+        Console.WriteLine("SUMMER = {0}", y);
+        Console.WriteLine("FALL = {0}", z);
+    }
+}
