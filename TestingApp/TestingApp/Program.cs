da8d078d9f6101752ff3677051d21a800af4f3ae
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-05 19:16:08 +0100

    added class program

---
+++ TestingApp/TestingApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class Student
+{
+    int Id;   
+    String Name; 
+
+    public static void Main(string[] args)
+    {
+        Student Std1 = new Student(); //creating an object of Student    
+        Std1.Id = 101;
+        Std1.Name = "John Doe";
+        Console.WriteLine(Std1.Id);
+        Console.WriteLine(Std1.Name);
+        Console.WriteLine("\n");
+
+    }
+}
--- TestingApp/TestingApp/Properties/AssemblyInfo.cs
+++ TestingApp/TestingApp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HelloWorld")]
+[assembly: AssemblyTitle("TestingApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HelloWorld")]
+[assembly: AssemblyProduct("TestingApp")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("04cf5637-d14e-4d86-80d0-a23b3d8b29c3")]
+[assembly: Guid("b5d8cdc8-d6ab-47a2-a86b-2e5442483f00")]
 
 // Version information for an assembly consists of the following four values:
 //
