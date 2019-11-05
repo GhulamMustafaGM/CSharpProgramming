40045384590617e4b976df8e5ce3de53345019fd
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-05 20:01:19 +0100

    added class8 this

---
+++ TestingApp/TestingApp/class8.cs
@@ -0,0 +1,28 @@
+﻿using System;
+public class Student
+{
+    public int StdId;
+    public String StdName;
+    public float salary;
+    public Student(int StdId, String StdName, float salary)
+    {
+        this.StdId = StdId;
+        this.StdName = StdName;
+        this.salary = salary;
+    }
+    public void display()
+    {
+        Console.WriteLine(StdId + " " + StdName + " " + salary);
+    }
+}
+class TestStudent
+{
+    public static void Main(string[] args)
+    {
+        Student S1 = new Student (104, "Jane Moon", 890000f);
+        Student S2 = new Student(102, "Jone Doe", 490000f);
+        S1.display();
+        S2.display();
+
+    }
+}
