7c59ec4f8cbbe39cb38617e30fcf46b7c72dc781
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-05 19:55:12 +0100

    added class7 Constructor and Destructor

---
+++ TestingApp/TestingApp/class7.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class Student
+{
+    public Student()
+    {
+        Console.WriteLine("Constructor Invoked \n");
+        
+    }
+    ~Student()
+    {
+        Console.WriteLine("Destructor Invoked \n");
+    }
+}
+class TestEmployee
+{
+    public static void Main(string[] args)
+    {
+        Student S1 = new Student();
+        Student S2 = new Student();
+        
+    }
+    
+}
