2690465093cdbaf8e16bd14dea2ebb945739477c
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-05 21:17:47 +0100

    added class22: having logic and setting value

---
+++ TestingApp/TestingApp/class22.cs
@@ -0,0 +1,27 @@
+﻿using System;
+public class Student
+{
+    private string stdname;
+
+    public string stdName
+    {
+        get
+        {
+            return stdname;
+        }
+        set
+        {
+            stdname = value + " at Linkoping University!";
+
+        }
+    }
+}
+class TestEmployee
+{
+    public static void Main(string[] args)
+    {
+        Student s1 = new Student();
+        s1.stdName = "William";
+        Console.WriteLine("Student Name: " + s1.stdName);
+    }
+}
