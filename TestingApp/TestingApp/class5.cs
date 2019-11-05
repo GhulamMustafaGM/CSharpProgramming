cae2b978c900376a8a8a528a56b9fe1a01f11bad
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-05 19:42:37 +0100

    added class default constructor

---
+++ TestingApp/TestingApp/class5.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public class Employee
+{
+    public Employee()
+    {
+        Console.WriteLine("Default Constructor Invoked \n");
+    }
+    public static void Main(string[] args)
+    {
+        Employee E1 = new Employee();
+        Employee E2 = new Employee();
+        Employee E3 = new Employee();
+    }
+}
