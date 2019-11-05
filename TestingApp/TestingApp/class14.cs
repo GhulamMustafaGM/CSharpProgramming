9bf6e2c8433079ed87c4b56de8043303a7e39fe4
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-05 20:22:31 +0100

    added class14 structure

---
+++ TestingApp/TestingApp/class14.cs
@@ -0,0 +1,16 @@
+﻿using System;
+public struct RectangleShape
+{
+    public int width, height;
+
+}
+public class TestStructs
+{
+    public static void Main()
+    {
+        RectangleShape r = new RectangleShape();
+        r.width = 4;
+        r.height = 5;
+        Console.WriteLine("Area of Rectangle is: " + (r.width * r.height));
+    }
+}
