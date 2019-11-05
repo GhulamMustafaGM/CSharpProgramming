a08179cc57e74d4a8d02e9be3215830714169a58
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-05 20:19:00 +0100

    added class13 static constructor

---
+++ TestingApp/TestingApp/class13.cs
@@ -0,0 +1,31 @@
+﻿using System;
+public class EAccount
+{
+    public int eid;
+    public String ename;
+    public static float rateOfInterest;
+    public EAccount(int eid, String ename)
+    {
+        this.eid = eid;
+        this.ename = ename;
+    }
+    static EAccount()
+    {
+        rateOfInterest = 9.5f;
+    }
+    public void display()
+    {
+        Console.WriteLine(eid + " " + ename + " " + rateOfInterest);
+    }
+}
+class TestEmployee
+{
+    public static void Main(string[] args)
+    {
+        EAccount E1 = new EAccount(1011, "John cluch");
+        EAccount E2 = new EAccount(1022, "Anderson Doe");
+        E1.display();
+        E2.display();
+
+    }
+}
