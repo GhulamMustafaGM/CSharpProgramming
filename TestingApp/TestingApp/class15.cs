7150fe85fd98e0888a797d1db7f21601873139ff
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-05 20:32:22 +0100

    added class15: Using Constructor and Method

---
+++ TestingApp/TestingApp/class15.cs
@@ -0,0 +1,25 @@
+﻿// Static field => Using Constructor and Method
+
+using System;
+public struct RectangleShape
+{
+    public int width, height;
+
+    public RectangleShape(int w, int h)
+    {
+        width = w;
+        height = h;
+    }
+    public void areaOfRectangle()
+    {
+        Console.WriteLine("Area of Rectangle is: " + (width * height));
+    }
+}
+public class TestStructs
+{
+    public static void Main()
+    {
+        RectangleShape r = new RectangleShape(6, 7);
+        r.areaOfRectangle();
+    }
+}
