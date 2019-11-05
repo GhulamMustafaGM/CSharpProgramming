c97afb412408d363ad0b6bb864062d316282b896
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-05 20:12:30 +0100

    added class11 static field and counting objects

---
+++ TestingApp/TestingApp/class11.cs
@@ -0,0 +1,34 @@
+﻿using System;
+public class EmpAccount
+{
+    public int accno;
+    public String name;
+    public static int count = 0;
+    public EmpAccount(int accno, String name)
+    {
+        this.accno = accno;
+        this.name = name;
+        count++;
+    }
+
+    public void display()
+    {
+        Console.WriteLine(accno + " " + name);
+    }
+}
+class TestAccount
+{
+    public static void Main(string[] args)
+    {
+        EmpAccount e1 = new EmpAccount(107, "Donald Duck");
+        EmpAccount e2 = new EmpAccount(108, "John Doe");
+        EmpAccount e3 = new EmpAccount(109, "Roland Carlsson");
+        
+        e1.display();
+        e2.display();
+        e3.display();
+        Console.WriteLine("\n");
+        Console.WriteLine("Total Objects are: " + EmpAccount.count);
+        Console.WriteLine("\n");
+    }
+}
