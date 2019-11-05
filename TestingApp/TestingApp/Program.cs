6eaecddaa2de9447d15eaca9c5b53e9a6af84e24
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-05 19:26:10 +0100

    added class2

--- TestingApp/TestingApp/Program.cs
+++ TestingApp/TestingApp/Program.cs
@@ -7,7 +7,7 @@ public class Student
     public static void Main(string[] args)
     {
         Student Std1 = new Student(); //creating an object of Student    
-        Std1.Id = 101;
+        Std1.Id = 103;
         Std1.Name = "John Doe";
         Console.WriteLine(Std1.Id);
         Console.WriteLine(Std1.Name);
---
+++ TestingApp/TestingApp/class2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+public class Student2
+{
+    public int id2;
+    public String name2;
+}
+class TestStudent
+{
+    public static void Main(string[] args)
+    {
+        Student2 s2 = new Student2();
+        s2.id2 = 104;
+        s2.name2 = "Jane Doe \n";
+        Console.WriteLine(s2.id2);
+        Console.WriteLine(s2.name2);
+
+    }
+}
