ae5d1c757f4ef00e65386a1f1b2dbe9080694b4d
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-05 06:20:57 +0100

    Added HelloWrold

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        private static int x;
+
+        static void Main(string[] args)
+        {
+            x = 10;
+
+            Console.WriteLine("The number is : ", x);
+        }
+    }
+}
---
+++ Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Class1
+{
+	public Class1()
+	{
+        int a = 10;
+
+        Console.WriteLine("heloo" + a);
+	}
+}
