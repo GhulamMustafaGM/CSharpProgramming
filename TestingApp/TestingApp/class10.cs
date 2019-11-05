584fcd8615c01e0e87d9799cbf7a77572c92007c
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-05 20:08:15 +0100

    added class10 static field2

---
+++ TestingApp/TestingApp/class10.cs
@@ -0,0 +1,29 @@
+﻿using System;
+public class EmpAccount
+{
+    public int accno;
+    public String name;
+    public static float rateOfInterest = 8.8f;
+    public EmpAccount(int accno, String name)
+    {
+        this.accno = accno;
+        this.name = name;
+    }
+
+    public void display()
+    {
+        Console.WriteLine(accno + " " + name + " " + rateOfInterest);
+    }
+}
+class TestAccount
+{
+    public static void Main(string[] args)
+    {
+        EmpAccount.rateOfInterest = 10.5f;//changing value  
+        EmpAccount E1 = new EmpAccount(103, "Davaid");
+        EmpAccount E2 = new EmpAccount(106, "Moses");
+        E1.display();
+        E2.display();
+
+    }
+}
