104b672b43c2e103099e8fa3d80c6894a4bed0fc
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-05 21:20:05 +0100

    added class23: read-only property

---
+++ TestingApp/TestingApp/class23.cs
@@ -0,0 +1,30 @@
+﻿using System;
+public class Employee
+{
+    private static int counter;
+
+    public Employee()
+    {
+        counter++;
+    }
+    public static int Counter
+    {
+        get
+        {
+            return counter;
+        }
+    }
+}
+class TestEmployee
+{
+    public static void Main(string[] args)
+    {
+        Employee E1 = new Employee();
+        Employee E2 = new Employee();
+        Employee E3 = new Employee();
+
+        //E1.Counter = 10;//Compile Time Error: Can't set value  
+
+        Console.WriteLine("No. of Employees: " + Employee.Counter);
+    }
+}
