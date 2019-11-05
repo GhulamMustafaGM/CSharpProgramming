5c1c7a09bca5538b3a58d530562be38677274fc6
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-05 21:12:46 +0100

    added class21: using properities

---
+++ TestingApp/TestingApp/class21.cs
@@ -0,0 +1,28 @@
+﻿using System;
+public class Employee
+{
+    private string name;
+
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+        set
+        {
+            name = value;
+        }
+    }
+}
+class TestEmployee
+{
+    public static void Main(string[] args)
+    {
+        Employee E1 = new Employee();
+        E1.Name = "Kuble Khan";
+        Console.WriteLine("Employee Name: " + E1.Name);
+        Console.WriteLine("\n");
+
+    }
+}
