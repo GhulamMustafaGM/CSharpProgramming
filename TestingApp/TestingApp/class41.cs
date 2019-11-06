a5abd6c18f72e5c5deb52b0983f91f2cba9d3fec
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-06 20:18:32 +0100

    added class41: namespace and by using keyword

---
+++ TestingApp/TestingApp/class41.cs
@@ -0,0 +1,29 @@
+﻿using System;  
+using One;  
+using Two;  
+
+namespace One
+{
+    public class Hej
+    {
+        public void sayHej() { Console.WriteLine("Hej Namespace"); }
+    }
+}
+
+namespace Two
+{
+    public class Welcome
+    {
+        public void sayWelcome() { Console.WriteLine("Welcome Namespace"); }
+    }
+}
+public class TestingNamespace
+{
+    public static void Main()
+    {
+        Hej h1 = new Hej();
+        Welcome w1 = new Welcome();
+        h1.sayHej();
+        w1.sayWelcome();
+    }
+}
