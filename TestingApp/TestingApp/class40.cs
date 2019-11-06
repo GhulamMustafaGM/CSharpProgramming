79787cbe5450262ab00dc81865c8b738f1a73e61
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-06 20:15:06 +0100

    added class40: namespace and by fully qualified name

---
+++ TestingApp/TestingApp/class40.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace FirstOne
+{
+    public class Hi
+    {
+        public void sayHi() { Console.WriteLine("Hi FirstOne Namespace"); }
+    }
+}
+namespace SecondOne
+{
+    public class Hi
+    {
+        public void sayHi() { Console.WriteLine("Hi SecondOne Namespace"); }
+    }
+}
+public class TestingNamespace
+{
+    public static void Main()
+    {
+        FirstOne.Hi h1 = new FirstOne.Hi();
+        SecondOne.Hi h2 = new SecondOne.Hi();
+        h1.sayHi();
+        h2.sayHi();
+
+    }
+}
