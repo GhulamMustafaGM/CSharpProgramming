a6be1cedf045678c4657f09011283ccd789faab9
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-06 22:34:59 +0100

    added class50: String Compare() Method

---
+++ TestingApp/TestingApp/class50.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string s1 = "Hi";
+        string s2 = "Hi";
+        string s3 = "World!";
+        string s4 = "Dear!";
+
+        Console.WriteLine(string.Compare(s1, s2));
+        Console.WriteLine(string.Compare(s2, s3));
+        Console.WriteLine(string.Compare(s3, s4));
+    }
+}
