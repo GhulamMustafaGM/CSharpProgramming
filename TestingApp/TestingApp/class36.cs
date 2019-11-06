d6030c385641d21929a91112c411acfe46d7221c
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-06 19:58:44 +0100

    added class36: Runtime Polymorphism with Data Members

---
+++ TestingApp/TestingApp/class36.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class Car
+{
+    public string color = "Black";
+
+}
+public class Toyota : Car
+{
+    public string color = "Gray";
+}
+public class TestingCar
+{
+    public static void Main()
+    {
+        Car c = new Toyota();
+        Toyota t = new Toyota();
+
+        Console.WriteLine(c.color);
+        Console.WriteLine(t.color);
+
+    }
+}
