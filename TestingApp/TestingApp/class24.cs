7eb6a29f4e447229347bbb252242724c1684e062
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-06 18:58:37 +0100

    added class24: Single line inheritance

---
+++ TestingApp/TestingApp/class24.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class Employee
+{
+    public float salary = 45000;
+}
+public class Programmer : Employee
+{
+    public float bonus = 12000;
+}
+class TestInheritance
+{
+    public static void Main(string[] args)
+    {
+        Programmer P1 = new Programmer();
+
+        Console.WriteLine("Salary: " + P1.salary);
+        Console.WriteLine("Bonus Salary: " + P1.bonus);
+
+    }
+}
