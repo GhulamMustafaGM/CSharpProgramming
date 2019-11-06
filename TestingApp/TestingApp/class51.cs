a097baf2e55366e4518154132d6c082ac2021e2b
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-06 22:38:10 +0100

    added class51: String CompareOrdinal() Method

---
+++ TestingApp/TestingApp/class51.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class StringExample
+{
+    public static void Main(string[] args)
+    {
+        string s1 = "Hi";
+        string s2 = "Hi";
+        string s3 = "World";
+        string s4 = "Dear";
+
+        Console.WriteLine(string.CompareOrdinal(s1, s2));
+        Console.WriteLine(string.CompareOrdinal(s1, s3));
+        Console.WriteLine(string.CompareOrdinal(s1, s4));
+    }
+}
