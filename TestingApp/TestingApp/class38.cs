cef7de126cb6a15f5882c3d4b93a822272a5f660
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-06 20:09:01 +0100

    added class38: Interface

---
+++ TestingApp/TestingApp/class38.cs
@@ -0,0 +1,31 @@
+﻿using System;
+public interface MainDrawable
+{
+    void draw();
+}
+public class Rectangle : MainDrawable
+{
+    public void draw()
+    {
+        Console.WriteLine("drawing rectangle...");
+    }
+}
+public class Circle : MainDrawable
+{
+    public void draw()
+    {
+        Console.WriteLine("drawing circle...");
+        Console.WriteLine("\n");
+    }
+}
+public class TestingInterface
+{
+    public static void Main()
+    {
+        MainDrawable md;
+        md = new Rectangle();
+        md.draw();
+        md = new Circle();
+        md.draw();
+    }
+}
