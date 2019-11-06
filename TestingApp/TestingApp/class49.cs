42c6f134e424e18a18a6bed22230850e48c6705a
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-06 22:32:25 +0100

    added class49: String Clone () method

---
+++ TestingApp/TestingApp/class49.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string s1 = "Hej ";
+        string s2 = (String)s1.Clone();
+        string s3 = (String)s1.Clone();
+        string s4 = (String)s1.Clone();
+
+        Console.WriteLine(s1);
+        Console.WriteLine(s2);
+        Console.WriteLine(s3);
+        Console.WriteLine(s4);
+    }
+}
