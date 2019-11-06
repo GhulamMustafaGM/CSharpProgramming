43b8911e5885d7a2d0b5328ac763d17de12bae69
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-06 19:02:13 +0100

    added class25: singleLine Inheritance

---
+++ TestingApp/TestingApp/class25.cs
@@ -0,0 +1,18 @@
+﻿using System;
+public class Animal
+{
+    public void eat() { Console.WriteLine("Eating..."); }
+}
+public class Cat : Animal
+{
+    public void Mew() { Console.WriteLine("Mew mew mew..."); }
+}
+class TestInheritance2
+{
+    public static void Main(string[] args)
+    {
+        Cat c1 = new Cat();
+        c1.eat();
+        c1.Mew();
+    }
+}
