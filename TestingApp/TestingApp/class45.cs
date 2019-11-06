cd0cbbcc4e7d18bc9007dc35bc1fa021b936eb5a
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-06 20:36:33 +0100

    added class45: Protected Internal Access Specifier

---
+++ TestingApp/TestingApp/class45.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AccessSpecifiers
+{
+    class InternalTest
+    {
+        protected internal string name = "Donald Duck";
+        protected internal void Msg(string msg)
+        {
+            Console.WriteLine("Hello " + msg);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InternalTest internalTest = new InternalTest();
+            // Accessing protected internal variable  
+            Console.WriteLine("Hello " + internalTest.name);
+            // Accessing protected internal function  
+            internalTest.Msg("Petersson Don");
+        }
+    }
+}
