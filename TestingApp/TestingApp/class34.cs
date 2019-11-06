19a5cb0c859d0a8f57eec8a2ff5a557d3dff3d18
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-06 19:49:27 +0100

    added class34:  Runtime Polymorphism

---
+++ TestingApp/TestingApp/class34.cs
@@ -0,0 +1,24 @@
+﻿using System;
+public class Animal
+{
+    public virtual void eat()
+    {
+        Console.WriteLine("eating...");
+    }
+}
+public class Cat : Animal
+{
+    public override void eat()
+    {
+        Console.WriteLine("eating bread...");
+    }
+
+}
+public class TestingPolymorphism
+{
+    public static void Main()
+    {
+        Animal a = new Cat();
+        a.eat();
+    }
+}
