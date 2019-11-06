e686fa27dbd48a2fb059ec32ba3fd366c7fcca86
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-06 19:25:51 +0100

    added class29: Member Overloading

---
+++ TestingApp/TestingApp/class29.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class Counting
+{
+    public static int add(int x, int y)
+    {
+        return x + y;
+    }
+    public static float add(float x, float y)
+    {
+        return x + y;
+    }
+}
+public class TestMemberOverloading
+{
+    public static void Main()
+    {
+        Console.WriteLine(Counting.add(10, 22));
+        Console.WriteLine(Counting.add(11.4f, 20.3f));
+    }
+}
