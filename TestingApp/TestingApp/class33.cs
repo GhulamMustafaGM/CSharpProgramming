cb87b87cc230bb08fae6927b754f5abc5e433fa5
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-06 19:47:14 +0100

    added class33: Inheritance and calling base class constructor internally

---
+++ TestingApp/TestingApp/class33.cs
@@ -0,0 +1,24 @@
+﻿using System;
+public class Animal
+{
+    public Animal()
+    {
+        Console.WriteLine("animal...");
+    }
+}
+public class Cat : Animal
+{
+    public Cat()
+    {
+        Console.WriteLine("cat...");
+    }
+
+}
+public class TestingOverriding
+{
+    public static void Main()
+    {
+        Cat c = new Cat();
+
+    }
+}
