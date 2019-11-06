e3eb87fb2667cba279710be5a7a6d7971227df04
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-06 19:42:39 +0100

    added class31: base keyword and accessing base class field

---
+++ TestingApp/TestingApp/class31.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class Animal
+{
+    public string color = "white";
+}
+public class Cat : Animal
+{
+    string color = "black";
+    public void showColor()
+    {
+        Console.WriteLine(base.color);
+        Console.WriteLine(color);
+    }
+
+}
+public class TestBase
+{
+    public static void Main()
+    {
+        Cat c = new Cat();
+        c.showColor();
+    }
+}
