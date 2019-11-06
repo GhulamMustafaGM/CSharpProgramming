423376b7934e629c7edc913a3325b51acc03e8ff
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-06 19:53:12 +0100

    added class35: Runtime Polymorphism

---
+++ TestingApp/TestingApp/class35.cs
@@ -0,0 +1,38 @@
+﻿using System;
+public class MainShape
+{
+    public virtual void draw()
+    {
+        Console.WriteLine("drawing...");
+    }
+}
+public class Rectangle : MainShape
+{
+    public override void draw()
+    {
+        Console.WriteLine("drawing rectangle...");
+    }
+
+}
+public class Circle : MainShape
+{
+    public override void draw()
+    {
+        Console.WriteLine("drawing circle...");
+    }
+
+}
+public class TestingPolymorphism
+{
+    public static void Main()
+    {
+        MainShape ms;
+        ms = new MainShape();
+        ms.draw();
+        ms = new Rectangle();
+        ms.draw();
+        ms = new Circle();
+        ms.draw();
+
+    }
+}
