7e5ade93b700e697ae3f2868497319bd9a5a07b3
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-06 19:05:28 +0100

    added class: multi inheritance fields

---
+++ TestingApp/TestingApp/class26.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class Animal
+{
+    public void eat() { Console.WriteLine("Eating..."); }
+}
+public class Cat : Animal
+{
+    public void Mew() { Console.WriteLine("Mew mow mow..."); }
+}
+public class BabyCat : Cat
+{
+    public void weep() { Console.WriteLine("Weeping..."); }
+}
+class TestInheritance2
+{
+    public static void Main(string[] args)
+    {
+        BabyCat c1 = new BabyCat();
+        c1.eat();
+        c1.Mew();
+        c1.weep();
+    }
+}
