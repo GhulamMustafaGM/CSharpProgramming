2f42d90c691e4007990ad325eae5cd815e12fe8a
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-06 20:04:19 +0100

    added class37: Abstract class

---
+++ TestingApp/TestingApp/class37.cs
@@ -0,0 +1,31 @@
+﻿using System;
+public abstract class MainShape
+{
+    public abstract void draw();
+}
+public class Rectangle : MainShape
+{
+    public override void draw()
+    {
+        Console.WriteLine("drawing rectangle...");
+    }
+}
+public class Circle : MainShape
+{
+    public override void draw()
+    {
+        Console.WriteLine("drawing circle...");
+        Console.WriteLine("\n");
+    }
+}
+public class TestingAbstract
+{
+    public static void Main()
+    {
+        MainShape ms;
+        ms = new Rectangle();
+        ms.draw();
+        ms = new Circle();
+        ms.draw();
+    }
+}
