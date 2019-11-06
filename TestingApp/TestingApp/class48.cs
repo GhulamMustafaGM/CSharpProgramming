94bf2171d5f7713d25c1d56f47c6b97a3bb378e8
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-06 22:24:54 +0100

    class48: String

---
+++ TestingApp/TestingApp/class48.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string s1 = "hey";
+
+        char[] ch = { 'W', 'o', 'r', 'd', '!' };
+        string s2 = new string(ch);
+
+        Console.WriteLine(s1);
+        Console.WriteLine(s2);
+    }
+}
