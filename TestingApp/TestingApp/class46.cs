1e77c13082fba58d24c15660b9272e02749d6065
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-06 20:38:23 +0100

    added class46: Private Access Specifier

---
+++ TestingApp/TestingApp/class46.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace AccessSpecifiers
+{
+    class Program
+    {
+        private string name = "Ricksson Roland";
+        private void Msg(string msg)
+        {
+            Console.WriteLine("Hello " + msg);
+        }
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            // Accessing private variable  
+            Console.WriteLine("Hello " + program.name);
+            // Accessing private function  
+            program.Msg("Tony Byre");
+        }
+    }
+}
