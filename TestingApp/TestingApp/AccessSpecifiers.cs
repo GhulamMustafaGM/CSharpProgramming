1e74ce210e378079f5c9c71d94cc0d8d9212db58
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-06 20:47:10 +0100

    added class 47: Encapsulation

---
+++ TestingApp/TestingApp/AccessSpecifiers.cs
@@ -0,0 +1,10 @@
+﻿namespace AccessSpecifiers
+{
+    class Student
+    {
+        // Creating setter and getter for each property  
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ TestingApp/TestingApp/class47.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AccessSpecifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+
+            // Setting values to the properties  
+            student.ID = "102";
+            student.Name = "James Sand";
+            student.Email = "James.Sand@email.com";
+
+            // getting values  
+            Console.WriteLine("ID = " + student.ID);
+            Console.WriteLine("Name = " + student.Name);
+            Console.WriteLine("Email = " + student.Email);
+        }
+    }
+}
