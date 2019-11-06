c2aa0ba2a9106610c0ffc276505a4228f79b3f00
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-06 20:26:36 +0100

    added class42: Public Access Specifier

---
+++ TestingApp/TestingApp/class42.cs
@@ -0,0 +1,26 @@
+﻿// Access modifiers or specifiers, five have consisted public, protected, internal, protected internal, private
+
+
+using System;
+namespace AccessModifiers
+{
+    class PublicTest
+    {
+        public string name = "John Doe";
+        public void Msg(string msg)
+        {
+            Console.WriteLine("Hello " + msg);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PublicTest publicTest = new PublicTest();
+            // Accessing public variable  
+            Console.WriteLine("Hello " + publicTest.name);
+            // Accessing public function  
+            publicTest.Msg("Anderson anders");
+        }
+    }
+}
