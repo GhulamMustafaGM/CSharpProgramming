8680e5129f7a933b9843bdfbeb3368621086999f
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-06 19:45:08 +0100

    added class32: base keyword and calling base class method

---
+++ TestingApp/TestingApp/class32.cs
@@ -0,0 +1,25 @@
+﻿using System;
+public class Animal
+{
+    public virtual void eat()
+    {
+        Console.WriteLine("eating...");
+    }
+}
+public class Cat : Animal
+{
+    public override void eat()
+    {
+        base.eat();
+        Console.WriteLine("eating bread...");
+    }
+
+}
+public class TestBase
+{
+    public static void Main()
+    {
+        Cat c = new Cat();
+        c.eat();
+    }
+}
