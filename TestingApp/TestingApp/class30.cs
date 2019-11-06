c9f00204d9e69cfe06adc5918f7d4e6294d1817a
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-06 19:37:09 +0100

    added class30: Method Overriding

---
+++ TestingApp/TestingApp/class30.cs
@@ -0,0 +1,23 @@
+﻿using System;
+public class Animal
+{
+    public virtual void eat()
+    {
+        Console.WriteLine("Eating...");
+    }
+}
+public class Cat : Animal
+{
+    public override void eat()
+    {
+        Console.WriteLine("Eating bread...");
+    }
+}
+public class TestOverriding
+{
+    public static void Main()
+    {
+        Cat c = new Cat();
+        c.eat();
+    }
+}
