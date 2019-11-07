5ce202442d1101268ab7a236ced6b89f829ee78c
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 17:22:08 +0100

    added class53: String Concat() Method

---
+++ TestingApp/TestingApp/class53.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class StringProgram
+
+{
+    public static void Main(string[] args)
+    {
+        string S1 = "Hej ";
+        string S2 = "World!";
+
+        Console.WriteLine(string.Concat(S1, S2));
+  
+    }
+}
