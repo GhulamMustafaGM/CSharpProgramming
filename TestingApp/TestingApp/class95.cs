1b2721c99e5dcebda9c2ea3b287df0cea0f8764d
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 20:05:46 +0100

    added class95: Exception

---
+++ TestingApp/TestingApp/class95.cs
@@ -0,0 +1,14 @@
+﻿// try/catch  
+
+using System;
+public class ExProgram
+{
+    public static void Main(string[] args)
+    {
+        int x = 10;
+        int y = 0;
+        int z = x / y;
+
+        Console.WriteLine("Rest of the code");
+    }
+}
