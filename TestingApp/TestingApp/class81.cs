ed5b026e015da0d634cbe8c6402ad5794aa4bea2
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 19:40:03 +0100

    added class81: string Replace() Method

---
+++ TestingApp/TestingApp/class81.cs
@@ -0,0 +1,13 @@
+﻿// String Replace() Method 
+
+using System;
+using System.Text;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string s5 = "Hello C#";
+        string s6 = s5.Replace('C', 'F');
+        Console.WriteLine(s6);
+    }
+}
