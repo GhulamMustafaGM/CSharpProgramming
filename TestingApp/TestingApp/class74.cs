5b888516a3b41a95011552306e69f81f6482bef0
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 19:17:32 +0100

    added class74: string IndexOf() vs LastIndexOf()

---
+++ TestingApp/TestingApp/class74.cs
@@ -0,0 +1,17 @@
+﻿// String IndexOf() vs LastIndexOf()
+
+using System;
+using System.Text;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string s3 = "Hello C#";
+
+        int first = s3.IndexOf('l');
+        int last = s3.LastIndexOf('l');
+
+        Console.WriteLine(first);
+        Console.WriteLine(last);
+    }
+}
