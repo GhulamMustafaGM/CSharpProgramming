f48ccea273db3f11f37de3b84f0dafc3e7c0762d
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 19:14:03 +0100

    added class73: string LastIndexOf()

---
+++ TestingApp/TestingApp/class73.cs
@@ -0,0 +1,14 @@
+﻿// String LastIndexOf()
+
+using System;
+using System.Text;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string s3 = "Hello C#";
+        int index = s3.LastIndexOf('l');
+
+        Console.WriteLine(index);
+    }
+}
