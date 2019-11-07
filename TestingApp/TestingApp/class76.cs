1455f2c2c33c9d1640a7c26dde95c0e211d8a65d
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 19:21:11 +0100

    added class76: string LastIndexOfAny() Method

---
+++ TestingApp/TestingApp/class76.cs
@@ -0,0 +1,15 @@
+﻿// String LastIndexOfAny() Method
+
+using System;
+using System.Text;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string s1 = "abracadabra";
+        char[] ch = { 't', 'b' };
+        int index = s1.LastIndexOfAny(ch);  //Finds 'b' at the last  
+
+        Console.WriteLine(index);
+    }
+}
