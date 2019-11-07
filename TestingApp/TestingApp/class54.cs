ff82f412d17d917eed0679eac897f2318e11cd72
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 17:26:06 +0100

    added class54: string containes()

---
+++ TestingApp/TestingApp/class54.cs
@@ -0,0 +1,18 @@
+﻿// C# String contains()
+
+using System;
+
+public class StringProgram
+
+{
+    public static void Main(string[] args)
+    {
+        string S1 = "Hej ";
+        string S2 = "World!";
+        string S3 = "Hi";
+
+        Console.WriteLine(S1.Contains(S2));
+        Console.WriteLine(S1.Contains(S3));
+
+    }
+}
