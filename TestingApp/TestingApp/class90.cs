2a37054a9cfa7216f64a1f72c6f142e2b0a38106
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 19:56:21 +0100

    added class90: String ToUpper() Method

---
+++ TestingApp/TestingApp/class90.cs
@@ -0,0 +1,14 @@
+﻿// String ToUpper() Method
+
+using System;
+using System.Text;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string s1 = "Hello C#";
+        string s3 = s1.ToUpper();
+
+        Console.WriteLine(s3);
+    }
+}
