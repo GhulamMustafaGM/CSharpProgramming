d6af8fa2d416bca59faf246ab25791d7d1cddefc
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 18:08:51 +0100

    added class59: GetEnumerator()

---
+++ TestingApp/TestingApp/class59.cs
@@ -0,0 +1,17 @@
+﻿// String GetEnumerator()
+
+using System;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string s3 = "Hej C#";
+        CharEnumerator ch = s3.GetEnumerator();
+
+        while(ch.MoveNext())
+        {
+            Console.WriteLine(ch.Current);
+        }
+        
+    }
+}
