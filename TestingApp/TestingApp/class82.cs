64bbd94679f4441eb995830659bf53b62bd89a71
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 19:41:23 +0100

    added class82: string replace() method

---
+++ TestingApp/TestingApp/class82.cs
@@ -0,0 +1,13 @@
+﻿// String Replace() Method 
+
+using System;
+using System.Text;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string s1 = "Hello C#, Hello .Net, Hello JavaScript";
+        string s2 = s1.Replace("Hello", "Cheers");
+        Console.WriteLine(s2);
+    }
+}
