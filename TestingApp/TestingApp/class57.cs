8b9cf57ef40fc5e29bd742234aa2780521b0762c
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 17:55:53 +0100

    added class57: String Equals()

---
+++ TestingApp/TestingApp/class57.cs
@@ -0,0 +1,14 @@
+﻿// String Equals()
+using System;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string s1 = "Hello";
+        string s2 = "Hello";
+        string s3 = "Bye";
+
+        Console.WriteLine(s1.Equals(s2));
+        Console.WriteLine(s1.Equals(s3));
+    }
+}
