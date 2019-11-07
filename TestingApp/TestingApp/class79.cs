259f043d4b3943b6a85391a025b609bb92b3ad36
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 19:36:49 +0100

    added class79: String Remove() Method

---
+++ TestingApp/TestingApp/class79.cs
@@ -0,0 +1,14 @@
+﻿// String Remove() Method 
+
+using System;
+using System.Text;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string s2 = "Hello C#";
+        string s3 = s2.Remove(2);
+
+        Console.WriteLine(s3);
+    }
+}
