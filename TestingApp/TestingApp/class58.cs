53ad97ba794f4bc72c2973d5911bfb2cca876a05
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 18:05:46 +0100

    added class58: string format()

---
+++ TestingApp/TestingApp/class58.cs
@@ -0,0 +1,12 @@
+﻿// String Format()
+
+using System;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+       string s1 = string.Format("{0:D}", DateTime.Now);
+
+        Console.WriteLine(s1);
+    }
+}
