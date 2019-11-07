8b4e78b24cb44521bda013734ea5bc8ec869aa20
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 19:51:03 +0100

    added class86: String ToCharArray() Method

---
+++ TestingApp/TestingApp/class86.cs
@@ -0,0 +1,16 @@
+﻿// String ToCharArray()
+
+using System;
+using System.Text;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string s1 = "Hello C#";
+        char[] ch = s1.ToCharArray();
+        foreach (char c in ch)
+        {
+            Console.WriteLine(c);
+        }
+    }
+}
