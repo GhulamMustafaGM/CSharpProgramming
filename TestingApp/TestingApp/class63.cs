87ea22400be1c0cdbd8d32356762e0040a75130b
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 18:18:14 +0100

    added class63: string IndexOf() method

---
+++ TestingApp/TestingApp/class63.cs
@@ -0,0 +1,14 @@
+﻿// String IndexOf() method
+
+using System;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string s1 = "Hello hello C#";
+        int index = s1.IndexOf('e');
+
+        Console.WriteLine(index);
+
+    }
+}
