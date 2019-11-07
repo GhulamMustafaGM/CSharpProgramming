ec707c3ae1fd96b218c79045b18ace71fbcb248b
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 19:12:03 +0100

    added class72:  String Join()

---
+++ TestingApp/TestingApp/class72.cs
@@ -0,0 +1,14 @@
+﻿// String Join()
+
+using System;
+using System.Text;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string[] s1 = { "Hello", "C#", "by", "javatpoint" };
+        string s3 = string.Join("-", s1);
+
+        Console.WriteLine(s3);
+    }
+}
