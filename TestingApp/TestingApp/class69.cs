cab4b983b22a2829605794a90f5912f66223c726
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 18:44:42 +0100

    added class69: string normalize() method

---
+++ TestingApp/TestingApp/class69.cs
@@ -0,0 +1,14 @@
+﻿// String Normalize() method
+
+using System;
+using System.Text;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string s1 = "Hello C#";
+        bool b1 = s1.IsNormalized();
+        Console.WriteLine(s1);
+        Console.WriteLine(b1);
+    }
+}
