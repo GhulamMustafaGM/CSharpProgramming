280770b44fca512c1ac61809bab464f283d599e8
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 18:31:08 +0100

    added class67: String Intern() vs IsInterned() method

---
+++ TestingApp/TestingApp/class67.cs
@@ -0,0 +1,18 @@
+﻿// String Intern() vs IsInterned() method
+
+using System;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string x = new string(new[] { 'x' });
+        string y = new string(new[] { 'y' });
+
+        string.Intern(x); // interns it
+        Console.WriteLine(string.IsInterned(x) != null); // true
+
+        string.IsInterned(y); // doesn't intern it
+
+        Console.WriteLine(string.IsInterned(y) != null); // false
+    }
+}
