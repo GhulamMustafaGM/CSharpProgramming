51da71d4ad01b178135d481d07a687818e5e1568
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 20:17:46 +0100

    added class101: Unchecked

---
+++ TestingApp/TestingApp/class101.cs
@@ -0,0 +1,17 @@
+﻿// Unchecked
+
+using System;
+namespace CSharpProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            unchecked
+            {
+                int val = int.MaxValue;
+                Console.WriteLine(val + 2);
+            }
+        }
+    }
+}
