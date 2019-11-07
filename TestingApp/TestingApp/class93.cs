c8d08bd7c9ae3aab197c2535c9bde2e2d3f88e6b
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 20:00:17 +0100

    added class93: StringTrimEnd() Method

---
+++ TestingApp/TestingApp/class93.cs
@@ -0,0 +1,15 @@
+﻿// String TrimEnd() Method
+
+using System;
+using System.Text;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string s1 = "Hello C#";
+        char[] ch = { '#' };
+        string s3 = s1.Trim(ch);
+
+        Console.WriteLine(s3);
+    }
+}
