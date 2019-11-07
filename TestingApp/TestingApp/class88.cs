d89a0725bccf8b2708f3b105b128554ef83f3768
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 19:53:46 +0100

    added class88: String ToLowerInvariant() Method

---
+++ TestingApp/TestingApp/class88.cs
@@ -0,0 +1,14 @@
+﻿// String ToLowerInvariant() Method
+
+using System;
+using System.Text;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string s1 = "Hello C#";
+        string s2 = s1.ToLowerInvariant();
+
+        Console.WriteLine(s2);
+    }
+}
