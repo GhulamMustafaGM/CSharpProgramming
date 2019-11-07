3c28a25ec4b56e1bd7fa490c5106951a58b5772a
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 20:10:58 +0100

    finally program if exception is not handled

---
+++ TestingApp/TestingApp/class97.cs
@@ -0,0 +1,18 @@
+﻿// try/catch/finally 
+
+using System;
+public class ExProgram
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            int x = 10;
+            int y = 0;
+            int z = x / y;
+        }
+        catch (NullReferenceException e) { Console.WriteLine(e); }
+        finally { Console.WriteLine("Finally block is executed"); }
+        Console.WriteLine("Rest of the code");
+    }
+}
