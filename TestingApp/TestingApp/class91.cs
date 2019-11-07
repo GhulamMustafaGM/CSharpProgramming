41493dfc84cfe06e164155e6f4a41c2197a53b12
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 19:57:35 +0100

    added class91: String ToUpperInvariant() Method

---
+++ TestingApp/TestingApp/class91.cs
@@ -0,0 +1,14 @@
+﻿// String ToUpperInvariant() Method
+
+using System;
+using System.Text;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string s1 = "Hello C#";
+        string s3 = s1.ToUpperInvariant();
+
+        Console.WriteLine(s3);
+    }
+}
