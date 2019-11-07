9fcdfac5425e28d10cfe267e79b788a0a0de8999
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 17:28:40 +0100

    added class55: String Copy() Method

---
+++ TestingApp/TestingApp/class55.cs
@@ -0,0 +1,17 @@
+﻿// String Copy() Method 
+
+using System;
+
+public class StringProgram
+
+{
+    public static void Main(string[] args)
+    {
+        string S1 = "Hej ";
+        string S2 = string.Copy(S1);
+
+        Console.WriteLine(S1);
+        Console.WriteLine(S2);
+
+    }
+}
