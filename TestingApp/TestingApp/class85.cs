52c81738795736d44f0f287c9b091dd0c7f349fa
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 19:49:45 +0100

    added class85: String SubString() Method

---
+++ TestingApp/TestingApp/class85.cs
@@ -0,0 +1,14 @@
+﻿// String SubString()
+
+using System;
+using System.Text;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string s1 = "Hello C Sharp";
+        string s2 = s1.Substring(5);
+
+        Console.WriteLine(s2);
+    }
+}
