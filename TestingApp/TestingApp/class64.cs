4d25f8bb77daef731c25d3c72059d0fb671886fe
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 18:20:55 +0100

    added class64: string insert() method

---
+++ TestingApp/TestingApp/class64.cs
@@ -0,0 +1,13 @@
+﻿// String Insert() method
+
+using System;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string s1 = "Hello hello C#";
+        string s3 = s1.Insert(5, "-");
+
+        Console.WriteLine(s3);
+    }
+}
