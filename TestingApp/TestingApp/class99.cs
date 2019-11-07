25a5a4674bbbb76b2fadbb1ac551747159db6b0e
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 20:15:30 +0100

    added class99: Checked awithout using checked

---
+++ TestingApp/TestingApp/class99.cs
@@ -0,0 +1,14 @@
+﻿// Checked awithout using checked 
+
+using System;
+namespace CSharpProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int val = int.MaxValue;
+            Console.WriteLine(val + 2);
+        }
+    }
+}
