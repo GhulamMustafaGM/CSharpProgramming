5f85241f360fc69148dffe5650f61277f5caa166
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 19:52:27 +0100

    added class87: String ToLower() Method

---
+++ TestingApp/TestingApp/class87.cs
@@ -0,0 +1,14 @@
+﻿// String String ToLower()
+
+using System;
+using System.Text;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string s1 = "Hello C#";
+        string s2 = s1.ToLower();
+
+        Console.WriteLine(s2);
+    }
+}
