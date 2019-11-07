c5fc6203334f792409c709f7cee90e1e45efcf86
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 19:33:05 +0100

    added class77: String PadLeft() Method

---
+++ TestingApp/TestingApp/class77.cs
@@ -0,0 +1,14 @@
+﻿// String PadLeft() Method 
+
+using System;
+using System.Text;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string s1 = "Hello C#";// 8 length of characters  
+        string s2 = s1.PadLeft(10);//(10-8=2) adds 2 whitespaces at the left side  
+
+        Console.WriteLine(s2);
+    }
+}
