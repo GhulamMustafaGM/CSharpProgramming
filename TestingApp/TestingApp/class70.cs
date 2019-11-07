3a6229b4e684bff17817f7ae47f79f489c6d3174
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 18:47:46 +0100

    added class70: string nomalize() method

---
+++ TestingApp/TestingApp/class70.cs
@@ -0,0 +1,17 @@
+﻿// String Normalize() method
+
+using System;
+using System.Text;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string s1 = "Hello C#";
+        string s2 = "";
+        bool b1 = string.IsNullOrEmpty(s1);
+        bool b2 = string.IsNullOrEmpty(s2);
+
+        Console.WriteLine(b1);
+        Console.WriteLine(b2);
+    }
+}
