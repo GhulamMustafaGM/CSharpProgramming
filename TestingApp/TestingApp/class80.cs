09ab14216bf58870079367d535b8a0fd430c71c5
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 19:38:09 +0100

    added class80: String Remove() Method

---
+++ TestingApp/TestingApp/class80.cs
@@ -0,0 +1,14 @@
+﻿// String Remove() Method 
+
+using System;
+using System.Text;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string s2 = "abcdefghijklmn";
+        string s3 = s2.Remove(5,6);
+
+        Console.WriteLine(s3);
+    }
+}
