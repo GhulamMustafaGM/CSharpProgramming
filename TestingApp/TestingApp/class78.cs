2e4f2e2c8cbabe7ae26d9edcbd60412747f19d35
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 19:34:47 +0100

    added class79: String PadRight() Method

---
+++ TestingApp/TestingApp/class78.cs
@@ -0,0 +1,14 @@
+﻿// String PadRight() Method 
+
+using System;
+using System.Text;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string s1 = "Hello C#";// 8 length of characters  
+        string s2 = s1.PadRight(15);
+        Console.Write(s2);//padding at right side (15-8=7)  
+        Console.Write("JavaScript ");//will be written after 7 white spaces  
+    }
+}
