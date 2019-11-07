7bd46059191c4b999938ad6befbdeb81e6218fbf
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 19:48:19 +0100

    added  class84: string startWith() Method

---
+++ TestingApp/TestingApp/class84.cs
@@ -0,0 +1,16 @@
+﻿// String StartsWith()
+
+using System;
+using System.Text;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string s1 = "Hello C Sharp";
+        bool b1 = s1.StartsWith("h");
+        bool b2 = s1.StartsWith("H");
+
+        Console.WriteLine(b1);
+        Console.WriteLine(b2);
+    }
+}
