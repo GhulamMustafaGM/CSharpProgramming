014b5ab71d3b337209262124449b8083eee39694
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 18:23:16 +0100

    added class65: string intern() method

---
+++ TestingApp/TestingApp/class65.cs
@@ -0,0 +1,14 @@
+﻿// String Intern(String str) method
+
+using System;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string s1 = "Hello hello C#";
+        string s3 = string.Intern(s1);
+
+        Console.WriteLine(s1);
+        Console.WriteLine(s3);
+    }
+}
