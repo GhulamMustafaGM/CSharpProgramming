507bf26378846659676eb609101197222646e535
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 19:46:53 +0100

    added class83: string Split() method

---
+++ TestingApp/TestingApp/class83.cs
@@ -0,0 +1,17 @@
+﻿// String Split() Method 
+
+using System;
+using System.Text;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string s1 = "Hello C Sharp";
+        string[] s2 = s1.Split(' ');
+
+        foreach (string s5 in s2)
+        {
+            Console.WriteLine(s5);
+        }
+    }
+}
