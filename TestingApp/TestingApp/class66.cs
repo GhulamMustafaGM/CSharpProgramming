52902414388c5ec453058515a32db80847546d82
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-07 18:25:51 +0100

    added class66: String IsInterned() method

---
+++ TestingApp/TestingApp/class66.cs
@@ -0,0 +1,16 @@
+﻿// String IsInterned() method
+
+using System;
+public class StringProgram
+{
+    public static void Main(string[] args)
+    {
+        string s1 = "Hello hello C#";
+        string s3 = string.Intern(s1);
+        string s4 = string.IsInterned(s1);
+
+        Console.WriteLine(s1);
+        Console.WriteLine(s3);
+        Console.WriteLine(s4);
+    }
+}
