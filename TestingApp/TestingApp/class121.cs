a6a90adc11ff50674d007b9017c28c9d28b8e426
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-08 20:45:19 +0100

    added class121: reflection

---
+++ TestingApp/TestingApp/class121.cs
@@ -0,0 +1,18 @@
+﻿// Reflection Program
+
+using System;
+using System.Reflection;
+public class ReflectionProgram
+{
+    public static void Main()
+    {
+        Type t = typeof(System.String);
+
+        Console.WriteLine("Methods of {0} type...", t);
+        MethodInfo[] ci = t.GetMethods(BindingFlags.Public | BindingFlags.Instance);
+        foreach (MethodInfo m in ci)
+        {
+            Console.WriteLine(m);
+        }
+    }
+}
