8bba2ec4c8047e87c888be08f6b3c62c8fa5eae3
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-08 20:23:20 +0100

    added class112: SortedDictionary

---
+++ TestingApp/TestingApp/class112.cs
@@ -0,0 +1,29 @@
+﻿// SortedDictionary<TKey, TValue>
+using System;
+using System.Collections.Generic;
+
+public class SortedLinkedListProgram
+{
+    public static void Main(string[] args)
+    {
+        SortedDictionary<string, string> names = new SortedDictionary<string, string>();
+        names.Add("1", "Shane");
+        names.Add("4", "Petersson");
+        names.Add("5", "Jane");
+        names.Add("3", "Roland");
+        names.Add("2", "Andersson");
+        foreach (KeyValuePair<string, string> kv in names)
+        {
+            Console.WriteLine(kv.Key + " " + kv.Value);
+        }
+    }
+}
+
+
+
+/*
+
+
+
+
+        */
