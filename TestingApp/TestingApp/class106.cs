939118afae6cdc29cdc41730f103ab8bfb3243c5
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-08 05:39:52 +0100

    added class106: SortedList

---
+++ TestingApp/TestingApp/class106.cs
@@ -0,0 +1,25 @@
+﻿// SortedSet <T>
+
+using System;
+using System.Collections.Generic;
+
+public class ListSetProgram
+{
+    public static void Main(string[] args)
+    {
+        // Create a set of strings  
+        var names = new SortedSet<string>();
+
+        names.Add("Roland");
+        names.Add("Andersson");
+        names.Add("Petersson");
+        names.Add("Ricky");
+        names.Add("Bush");  //will not be added  
+
+        // Iterate SortedSet elements using foreach loop  
+        foreach (var name in names)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
