21c6c54f221ae7ffd139dad37dea5c67ed2a2db1
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-08 20:54:50 +0100

    added class126: Threading Program: static method

---
+++ TestingApp/TestingApp/class126.cs
@@ -0,0 +1,24 @@
+﻿// Threading Program: static method
+
+using System;
+using System.Threading;
+public class ThreadProgram
+{
+    public static void Thread1()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+public class ThreadExample
+{
+    public static void Main()
+    {
+        Thread t1 = new Thread(new ThreadStart(ThreadProgram.Thread1));
+        Thread t2 = new Thread(new ThreadStart(ThreadProgram.Thread1));
+        t1.Start();
+        t2.Start();
+    }
+}
