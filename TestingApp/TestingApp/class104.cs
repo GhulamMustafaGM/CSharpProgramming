6a911e171b1872bf9b1f352b6d2fa8d872ce89c3
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-08 05:23:21 +0100

    added class105: list

---
+++ TestingApp/TestingApp/class104.cs
@@ -0,0 +1,21 @@
+﻿// List 
+using System;
+using System.Collections.Generic;
+
+public class ListProgram
+{
+    public static void Main(string[] args)
+    {
+        // Create a list of strings using collection initializer
+        var names = new List<string>
+            () { "Andersson", "John", "Daniel", "Carlsson"};
+            
+        // iterate through the list.
+            
+        // Iterate list element using foreach loop  
+        foreach (var name in names)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
