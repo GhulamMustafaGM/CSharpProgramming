5c4b840e8a9f9aa04afafc56998ac6d04c8ea801
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-08 20:27:46 +0100

    added class113: SortedLinkedList

---
+++ TestingApp/TestingApp/class113.cs
@@ -0,0 +1,21 @@
+﻿// SortedList
+
+using System;
+using System.Collections.Generic;
+
+public class SortedLinkedListProgram
+{
+    public static void Main(string[] args)
+    {
+        SortedList<string, string> names = new SortedList<string, string>();
+        names.Add("1", "Shane");
+        names.Add("4", "Petersson");
+        names.Add("5", "Jane");
+        names.Add("3", "Roland");
+        names.Add("2", "David");
+        foreach (KeyValuePair<string, string> kv in names)
+        {
+            Console.WriteLine(kv.Key + " " + kv.Value);
+        }
+    }
+}
