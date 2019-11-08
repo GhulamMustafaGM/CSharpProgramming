e65b6192fd0c4fd1700cb5d508f14a3dfcb6b7eb
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-08 20:01:17 +0100

    added class108: Stack

---
+++ TestingApp/TestingApp/class108.cs
@@ -0,0 +1,27 @@
+﻿// Stack<T>
+
+using System;
+using System.Collections.Generic;
+
+public class StackProgram
+{
+    public static void Main(string[] args)
+    {
+        Stack<string> names = new Stack<string>();
+        names.Push("Andersson");
+        names.Push("Petersson");
+        names.Push("Jane");
+        names.Push("Ricky");
+        names.Push("Donald");
+
+        foreach (string name in names)
+        {
+            Console.WriteLine(name);
+        }
+
+        Console.WriteLine("Peek element: " + names.Peek());
+        Console.WriteLine("Pop: " + names.Pop());
+        Console.WriteLine("After Pop, Peek element: " + names.Peek());
+
+    }
+}
