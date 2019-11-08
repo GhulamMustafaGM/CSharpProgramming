e4155f7207fb0d2a2ec934310c4986e98f349110
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-08 20:46:17 +0100

    added class122: reflection

---
+++ TestingApp/TestingApp/class122.cs
@@ -0,0 +1,18 @@
+﻿// Reflection Program
+
+using System;
+using System.Reflection;
+public class ReflectionProgram
+{
+    public static void Main()
+    {
+        Type t = typeof(System.String);
+
+        Console.WriteLine("Fields of {0} type...", t);
+        FieldInfo[] ci = t.GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.NonPublic);
+        foreach (FieldInfo f in ci)
+        {
+            Console.WriteLine(f);
+        }
+    }
+}
