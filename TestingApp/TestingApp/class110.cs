b7545092f88394c0e6fc2eb9f7ba54378457d728
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-08 20:07:43 +0100

    added class110: LinkedList

---
+++ TestingApp/TestingApp/class110.cs
@@ -0,0 +1,23 @@
+﻿// LinkedList 
+using System;
+using System.Collections.Generic;
+
+public class LinkedListProgram
+{
+    public static void Main(string[] args)
+    {
+        // Create a list of strings  
+        var names = new LinkedList<string>();
+        names.AddLast("Moses");
+        names.AddLast("Aderas");
+        names.AddLast("Petersson");
+        names.AddLast("John Duck");
+        names.AddFirst("Jane Doe");//added to first index  
+
+        // Iterate list element using foreach loop  
+        foreach (var name in names)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
