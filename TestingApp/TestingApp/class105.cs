771bdd70a60a39c83288955bf0fb01a1a824438d
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-08 05:36:06 +0100

    added class105: HashSet

---
+++ TestingApp/TestingApp/class105.cs
@@ -0,0 +1,25 @@
+﻿// HashSetProgram
+
+using System;
+using System.Collections.Generic;
+
+public class ListSetProgram
+{
+    public static void Main(string[] args)
+    {
+        // Create a set of strings  
+        var names = new HashSet<string>();
+
+        names.Add("John");
+        names.Add("Andersson");
+        names.Add("Karlssson");
+        names.Add("Daniel");
+        names.Add("Lara");  //will not be added  
+
+        // Iterate HashSet elements using foreach loop  
+        foreach (var name in names)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
