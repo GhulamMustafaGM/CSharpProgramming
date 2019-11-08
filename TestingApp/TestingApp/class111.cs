db47a0d3aa6e0bdda1405f9193572836ae67cd25
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-08 20:11:21 +0100

    added class111: LinkedList

---
+++ TestingApp/TestingApp/class111.cs
@@ -0,0 +1,27 @@
+﻿// LinkedList 
+using System;
+using System.Collections.Generic;
+
+public class LinkedListProgram
+{
+    public static void Main(string[] args)
+    {
+        // Create a list of strings  
+        var names = new LinkedList<string>();
+        names.AddLast("James");
+        names.AddLast("Anderson");
+        names.AddLast("Petersson");
+        names.AddLast("John");
+
+        //insert new element before "Peter"  
+        LinkedListNode<String> node = names.Find("Petersson");
+        names.AddBefore(node, "James");
+        names.AddAfter(node, "Petersson");
+
+        // Iterate list element using foreach loop  
+        foreach (var name in names)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
