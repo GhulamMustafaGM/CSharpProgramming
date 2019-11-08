068e6aad74df993c707b444aeb308586b32fa749
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-08 05:19:57 +0100

    added class103: List

---
+++ TestingApp/TestingApp/class103.cs
@@ -0,0 +1,23 @@
+﻿// List 
+using System;
+using System.Collections.Generic;
+
+public class ListProgram
+{
+    public static void Main(string[] args)
+    {
+        // Create a list of strings  
+        var names = new List<string>();
+
+        names.Add("Donald Duck");
+        names.Add("John Doe");
+        names.Add("Jane Doe");
+        names.Add("Anderson");
+
+        // Iterate list element using foreach loop  
+        foreach (var name in names)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
