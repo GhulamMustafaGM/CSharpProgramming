7b9e07ad163e8a2d78f24b04b743a724c51548e7
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-08 20:04:46 +0100

    added class109: Queue

---
+++ TestingApp/TestingApp/class109.cs
@@ -0,0 +1,26 @@
+﻿// Queue<T>
+
+using System;
+using System.Collections.Generic;
+
+public class QueueProgram
+{
+    public static void Main(string[] args)
+    {
+        Queue<string> names = new Queue<string>();
+        names.Enqueue("Andersson");
+        names.Enqueue("Petersson");
+        names.Enqueue("Tony");
+        names.Enqueue("Pointing");
+        names.Enqueue("David");
+
+        foreach (string name in names)
+        {
+            Console.WriteLine(name);
+        }
+
+        Console.WriteLine("Peek element: " + names.Peek());
+        Console.WriteLine("Dequeue: " + names.Dequeue());
+        Console.WriteLine("After Dequeue, Peek element: " + names.Peek());
+    }
+}
