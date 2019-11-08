fe97476a4e1cb4dc4e06d97498cedcdf64d7f49a
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-08 20:31:44 +0100

    added class115: Generic Method

---
+++ TestingApp/TestingApp/class115.cs
@@ -0,0 +1,23 @@
+﻿// Generic Method
+
+using System;
+namespace GenericProgram
+{
+    class GenericClass
+    {
+        public void Show<T>(T msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericClass genC = new GenericClass();
+            genC.Show("This is generic method");
+            genC.Show(101);
+            genC.Show('I');
+        }
+    }
+}
