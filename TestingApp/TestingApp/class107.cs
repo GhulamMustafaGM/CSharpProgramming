40ec240b9b16f4a1866130ffe830074de34f2951
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-08 05:43:38 +0100

    added class107: SortedSet

---
+++ TestingApp/TestingApp/class107.cs
@@ -0,0 +1,20 @@
+﻿// SortedSet <T>
+
+using System;
+using System.Collections.Generic;
+
+public class ListSetProgram
+{
+    public static void Main(string[] args)
+    {
+        // Create a set of strings  
+        var names = new SortedSet<string>()
+        { "David", "Moses", "Jack", "Ricky" };
+
+        // Iterate SortedSet elements using foreach loop  
+         foreach(var name in names)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
