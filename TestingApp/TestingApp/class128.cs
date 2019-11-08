f866543c71028d51d50df92920237b9597951cdc
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-08 20:58:48 +0100

    added class128: Threading - performing different tasks on each thread

---
+++ TestingApp/TestingApp/class128.cs
@@ -0,0 +1,26 @@
+﻿// Threading Program: performing different tasks on each thread
+
+using System;
+using System.Threading;
+
+public class MyThread
+{
+    public static void Thread1()
+    {
+        Console.WriteLine("task one");
+    }
+    public static void Thread2()
+    {
+        Console.WriteLine("task two");
+    }
+}
+public class ThreadExample
+{
+    public static void Main()
+    {
+        Thread t1 = new Thread(new ThreadStart(MyThread.Thread1));
+        Thread t2 = new Thread(new ThreadStart(MyThread.Thread2));
+        t1.Start();
+        t2.Start();
+    }
+}
