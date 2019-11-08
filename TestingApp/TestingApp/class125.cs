9f4378af6670ab49709f8f799e8aaaccea9cd080
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-08 20:50:54 +0100

    added class125: main thread

---
+++ TestingApp/TestingApp/class125.cs
@@ -0,0 +1,13 @@
+﻿// main thread
+
+using System;  
+using System.Threading;
+public class ThreadProgram
+{
+    public static void Main(string[] args)
+    {
+        Thread t = Thread.CurrentThread;
+        t.Name = "MainThread";
+        Console.WriteLine(t.Name);
+    }
+}
