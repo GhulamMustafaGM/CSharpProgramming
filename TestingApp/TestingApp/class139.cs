b8e5501aa4ef62f3cb384f736030027603abe2c8
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-10 22:43:32 +0100

    added class139: factorial

---
+++ TestingApp/TestingApp/class139.cs
@@ -0,0 +1,18 @@
+﻿// factorial program
+
+using System;
+public class FactorialProgram
+{
+    public static void Main(string[] args)
+    {
+        int a, fact = 1, number;
+        Console.Write("Enter any Number: ");
+        number = int.Parse(Console.ReadLine());
+        for (a = 1; a <= number; a++)
+        {
+            fact = fact * a;
+        }
+        Console.Write("Factorial of " + number + " is: " + fact);
+        Console.WriteLine("\n");
+    }
+}
