1cda89df795acc779f4311fce21f23d30a19bf72
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-11 18:07:00 +0100

    added class149: CSharp

---
+++ TestingApp/TestingApp/class149.cs
@@ -0,0 +1,17 @@
+﻿// CSharp program
+
+using System;
+
+namespace CShapdApp
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            /* my first program in C# */
+
+            Console.WriteLine("Welcome C# Programming!");
+            Console.ReadKey();
+        }
+    }
+}
