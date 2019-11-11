b16b86c592f61fcb4d690c9df604a79d5f31ec8a
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-11 18:21:46 +0100

    added class153: TypeConversion

---
+++ TestingApp/TestingApp/class153.cs
@@ -0,0 +1,26 @@
+﻿// TypeConversion
+
+using System;
+
+namespace TypeConversionApp
+{
+    class StringConversion
+    {
+        static void Main(string[] args)
+        {
+            int i = 76;
+            float f = 52.005f;
+            double d = 2445.7652;
+            bool b = true;
+
+            Console.WriteLine(i.ToString());
+            Console.WriteLine(f.ToString());
+            Console.WriteLine(d.ToString());
+            Console.WriteLine(b.ToString());
+            Console.ReadKey();
+            Console.ReadLine();
+
+        }
+
+    }
+}
