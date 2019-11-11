3c3c2aec25bca77c1fa5383955fd4c360310f9b8
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-11 18:16:21 +0100

    added class151: find integer size

---
+++ TestingApp/TestingApp/class151.cs
@@ -0,0 +1,16 @@
+﻿// sizeof program
+
+using System;
+
+namespace DataTypeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Size of int: {0}",
+                sizeof(int));
+            Console.ReadLine();
+        }
+    }
+}
