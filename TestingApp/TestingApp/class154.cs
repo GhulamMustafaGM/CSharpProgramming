73e774e5cc24a478fe8718aa2539f6865f322258
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-22 16:24:49 +0100

    added: class155: DeclaringConstants

---
+++ TestingApp/TestingApp/class154.cs
@@ -0,0 +1,25 @@
+﻿// CSharp Program
+
+using System;
+
+namespace VariableDefinitionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short a;
+            int b;
+            double c;
+         
+     
+            /* actual initialization */
+            a = 11;
+            b = 25;
+            c = a + b;
+            Console.WriteLine("a = {0}, b = {1}, c = {2}", a, b, c);
+           
+            Console.ReadLine();
+        }
+    }
+}
