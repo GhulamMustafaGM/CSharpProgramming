43af1363b16efabe6fa36c484a7b2dfdd8f70dcd
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-22 16:34:22 +0100

    added class156: Encapsulation

---
+++ TestingApp/TestingApp/class156.cs
@@ -0,0 +1,38 @@
+﻿// CSharp Program
+
+using System;
+
+namespace RectangleProgram
+{
+    class RectangleApp
+    {
+        //variables
+
+        public double length;
+        public double width;
+
+        public double GetArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+
+    }//end class Rectangle
+
+    class ExecuteRectangle
+    {
+        static void Main(string[] args)
+        {
+            RectangleApp r = new RectangleApp();
+            r.length = 4.5;
+            r.width = 3.5;
+            r.Display();
+            Console.ReadLine();
+        }
+    }
+}
