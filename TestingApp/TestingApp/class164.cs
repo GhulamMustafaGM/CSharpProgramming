b3b71175b7e7f28a2d5a4323f3d8789c77e31b34
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-22 17:08:14 +0100

    added class164: Array

---
+++ TestingApp/TestingApp/class164.cs
@@ -0,0 +1,28 @@
+﻿// Accessing Array Elements
+
+using System;
+
+namespace ArrayProgram
+{
+    class MyArray
+    {
+        static void Main(string[] args)
+        {
+            int[] n = new int[15]; /* n is an array of 15 integers */
+            int i, j;
+
+            /* initialize elements of array n */
+            for (i = 0; i < 15; i++)
+            {
+                n[i] = i + 100;
+            }
+
+            /* output each array element's value */
+            for (j = 0; j < 15; j++)
+            {
+                Console.WriteLine("Element[{0}] = {1}", j, n[j]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
