116c5723961fa0e66172abb780050138d6e4daf5
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-22 17:25:41 +0100

    added class168: String contains string

---
+++ TestingApp/TestingApp/class168.cs
@@ -0,0 +1,22 @@
+﻿// String Contains String
+
+using System;
+
+namespace StringProgram
+{
+
+    class StringProg
+    {
+
+        static void Main(string[] args)
+        {
+            string str = "This is test";
+
+            if (str.Contains("test"))
+            {
+                Console.WriteLine("The sequence 'test' was found.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
