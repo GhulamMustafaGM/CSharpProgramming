1f66a1910e161baba0cfd7337cc4a1de490437d5
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-22 16:44:19 +0100

    added class: Internal Access Specifier

---
+++ TestingApp/TestingApp/class158.cs
@@ -0,0 +1,37 @@
+﻿// Internal Access Specifier
+
+using System;
+
+namespace RectangleProgram
+{
+    class RectangleApp
+    {
+        //members variables declaration
+        internal double width;
+        internal double length;
+
+
+        double GetArea()
+        {
+            return length * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+    }//end class Rectangle
+
+    class ExecuteRectangle
+    {
+        static void Main(string[] args)
+        {
+            RectangleApp r = new RectangleApp();
+            r.length = 4.5;
+            r.width = 3.5;
+            r.Display();
+            Console.ReadLine();
+        }
+    }
+}
