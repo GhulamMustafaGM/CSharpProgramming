e0a32012f4c7ed67761e888080d21892723844f6
Ghulam Mustafa <mustafaji@gmail.com>
2019-11-22 17:28:40 +0100

    added class169: getting substring

---
+++ TestingApp/TestingApp/class169.cs
@@ -0,0 +1,19 @@
+﻿// Getting Substring
+
+using System;
+
+namespace StringProgram
+{
+
+    class StringProg
+    {
+
+        static void Main(string[] args)
+        {
+            string str = "Last night I dreamt of Pettersson";
+            Console.WriteLine(str);
+            string substr = str.Substring(23);
+            Console.WriteLine(substr);
+        }
+    }
+}
